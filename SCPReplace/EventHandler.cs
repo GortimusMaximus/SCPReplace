8769c7a8442b65699a7181db69f7d76c5d4695de
GortimusMaximus <aidhea137346@gmail.com>
2023-09-25 20:49:18 +0100

    Add project files.

---
+++ SCPReplace/EventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using PlayerRoles;
+
+namespace SCPReplace
+{
+    public class EventHandler
+    {
+        private bool RoundStarted = false;
+
+        public void OnRoundStarted()
+        {
+            RoundStarted = true;
+            Log.Debug("RoundStarted = true");
+        }
+
+        public void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            RoundStarted = false;
+            Log.Debug("RoundStarted = false");
+        }
+
+        public void OnPlayerLeaving(LeftEventArgs ev)
+        {
+            if (!RoundStarted || !ev.Player.IsScp) return;
+            Log.Debug("Player is an SCP");
+            Exiled.API.Features.Player player = Exiled.API.Features.Player.List.FirstOrDefault(x => x.Role == RoleTypeId.Spectator && x.UserId != ev.Player.UserId);
+            Log.Debug("Perameters passed");
+            if (player != null)
+            {
+                RoleTypeId scpID = ev.Player.Role;
+                Log.Debug("SCPID set");
+                player.Role.Set(scpID);
+                Log.Debug("Role set");
+                player.Position = ev.Player.Position;
+                Log.Debug("Position Set");
+                player.Rotation = ev.Player.Rotation;
+                Log.Debug("Rotation Set");
+                player.Health = ev.Player.Health;
+                Log.Debug("Health Set");
+                player.Broadcast(5, "You have replaced an scp");
+            }
+        }
+    }
+}
---
+++ SCPReplace/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPReplace
+{
+    public class Plugin : Plugin<Config>
+    {
+        private EventHandler eventHandler;
+
+        public override void OnEnabled()
+        {
+
+            eventHandler = new EventHandler();
+
+            Exiled.Events.Handlers.Server.RoundStarted += eventHandler.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded += eventHandler.OnRoundEnded;
+            Exiled.Events.Handlers.Player.Left += eventHandler.OnPlayerLeaving;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            eventHandler = null;
+
+            Exiled.Events.Handlers.Server.RoundStarted -= eventHandler.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded -= eventHandler.OnRoundEnded;
+            Exiled.Events.Handlers.Player.Left -= eventHandler.OnPlayerLeaving;
+            base.OnDisabled();
+        }
+
+    }
+}
